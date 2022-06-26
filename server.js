a91dc29a836da4bc4f2092f6ec3db4ad578eda25
Poseyd29 <poseyd29@gmail.com>
2022-06-26 03:55:12 -0400

    attempting first commit
    hopefully first commit has no issues

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+// const PORT = 5000
+
+app.listen(4000, () => {
+    console.log('listening on 4000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
