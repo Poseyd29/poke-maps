46bec1213813f838aed0504f19ef11b32c32a837
Poseyd29 <poseyd29@gmail.com>
2022-06-28 15:38:40 -0400

    Name and Stats available
    able to display name and stats for pokemon searched. unfortunately the page doesnt refresh to allow another search yet

--- public/main.js
+++ public/main.js
@@ -1,19 +1,10 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const app = express()
-
-console.log('hello world')
-
-
-
-
 
 const fetchPokemon = () => {
 
     document.querySelector('.search-button').addEventListener('click', fetchPokemon)
     const choice = document.querySelector('.search-input').value
 
-    const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+    const url = `https://pokeapi.co/api/v2/pokemon/${choice}/`
     // const url = `https://pokeapi.co/api/v2/encounter-method/${choice}`
     fetch(url)
         .then(res => {
@@ -23,11 +14,25 @@ const fetchPokemon = () => {
             const pokemon = {
                 name: data.name,
                 id: data.id,
-                image: data.sprites['front_default'],
-                locations: data.location_area_encounters
+                hp: data.stats[0].base_stat,
+                attack: data.stats[1].base_stat,
+                defense: data.stats[2].base_stat,
+                specialAttack: data.stats[3].base_stat,
+                specialDefense: data.stats[4].base_stat,
+                speed: data.stats[5].base_stat,
+
             }
+            console.log(data)
             console.log(pokemon)
-            document.querySelector('.screen').innerHTML = pokemon.locations
+            document.querySelector('.id').innerHTML = `ID: ${pokemon.id}`
+            document.querySelector('.name').innerHTML = `Name: ${pokemon.name}`
+            document.querySelector('.hp').innerHTML = `HP: ${pokemon.hp}`
+            document.querySelector('.attack').innerHTML = `Attack: ${pokemon.attack}`
+            document.querySelector('.defense').innerHTML = `Defense: ${pokemon.defense}`
+            document.querySelector('.special-attack').innerHTML = `Special Attack: ${pokemon.specialAttack}`
+            document.querySelector('.special-defense').innerHTML = `Special Defense: ${pokemon.specialDefense}`
+            document.querySelector('.speed').innerHTML = `Speed: ${pokemon.speed}`
+
         })
 }
 
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
 
 
+app.use(
+    cors({
+        origin: "*",
+    })
+)
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(4000, () => {
     console.log('listening on 4000')
 })
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
